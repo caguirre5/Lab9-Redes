916d264368d13c5822d8a9be20870a4db344de62
MarcoJurado20308 <60157453+MaJu502@users.noreply.github.com>
2023-11-09 22:51:19 -0600

    3.4 done

--- consumer.py
+++ consumer.py
@@ -4,6 +4,7 @@ import tkinter as tk
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 import json
+import struct
 
 # Configura el servidor y el topic de Kafka
 bootstrap_servers = 'lab9.alumchat.xyz:9092'
@@ -54,6 +55,35 @@ canvas.get_tk_widget().pack()
 
 # Función para iniciar el consumo de Kafka en un hilo
 
+# aqui haremos los cambios para el proceso de decode y encode para el ejercicio 3.4
+def encode_data(data):
+    encoded_temp = int((data['temperatura'] + 50) * 100)  # Rango de temperatura: -50 a 50
+    encoded_hume = int(data['humedad'])
+    # Codificar la dirección del viento
+    wind_directions = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE']
+    encoded_wind = wind_directions.index(data['direccion_viento'])
+
+    # Empaquetar los datos en 3 bytes (24 bits)
+    encoded_data = struct.pack('>HBB', encoded_temp, encoded_hume, encoded_wind)
+
+    return encoded_data
+
+# ... (código existente)
+
+def decode_data(encoded_data):
+    # Desempaquetar los datos
+    decoded_temp, decoded_hume, decoded_wind = struct.unpack('>HBB', encoded_data)
+
+    # Decodificar los datos
+    decoded_temp = (decoded_temp / 100) - 50  # Deshacer la codificación de temperatura
+    decoded_hume = int(decoded_hume)
+    # Decodificar la dirección del viento
+    wind_directions = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE']
+    decoded_wind = wind_directions[decoded_wind]
+
+    decoded_data = {'temperatura': decoded_temp, 'humedad': decoded_hume, 'direccion_viento': decoded_wind}
+    return decoded_data
+
 
 def start_kafka_consumer():
     try:
@@ -68,12 +98,13 @@ def start_kafka_consumer():
                 else:
                     print(f"Error en el mensaje: {message.error()}")
             else:
-                payload = json.loads(message.value())
-                all_temp.append(payload['temperatura'])
-                all_hume.append(payload['humedad'])
-                all_wind.append(payload['direccion_viento'])
+                # Decodificar el mensaje recibido
+                decoded_payload = decode_data(message.value())
+                all_temp.append(decoded_payload['temperatura'])
+                all_hume.append(decoded_payload['humedad'])
+                all_wind.append(decoded_payload['direccion_viento'])
 
-                print(f"Datos recibidos: {payload}")
+                print(f"Datos recibidos: {decoded_payload}")
 
                 # Grafica los datos en tiempo real
                 plot_all_data()
--- producer.py
+++ producer.py
@@ -2,6 +2,7 @@ from confluent_kafka import Producer
 import random
 import json
 import time
+import struct
 
 # Configura el servidor y el topic de Kafka
 bootstrap_servers = 'lab9.alumchat.xyz:9092'
@@ -15,9 +16,20 @@ producer_config = {
 
 producer = Producer(producer_config)
 
+# aqui vamos a poner la parte nueva de codigo para el ejercicio 3.4 donde codificamos la data
+def encode_data(data):
+    encoded_temp = int((data['temperatura'] + 50) * 100)  # Rango de temperatura: -50 a 50
+    encoded_hume = int(data['humedad'])
+    # Codificar la dirección del viento
+    wind_directions = ['N', 'NW', 'W', 'SW', 'S', 'SE', 'E', 'NE']
+    encoded_wind = wind_directions.index(data['direccion_viento'])
+
+    # Empaquetar los datos en 3 bytes (24 bits)
+    encoded_data = struct.pack('>HBB', encoded_temp, encoded_hume, encoded_wind)
+
+    return encoded_data
+
 # Función para generar datos aleatorios con distribución normal
-
-
 def generar_data():
     temperatura = round(random.gauss(50, 10), 1)  # Media 50, Varianza 10
     humedad = round(random.gauss(50, 10), 1)  # Media 50, Varianza 10
@@ -29,11 +41,14 @@ def generar_data():
 
 try:
     while True:
-        # Genera datos aleatorios
+        # Generar datos aleatorios
         data = generar_data()
 
-        # Envia los datos al servidor Kafka
-        producer.produce(topic, key='sensor20231', value=json.dumps(data))
+        # Codificar los datos antes de enviar
+        encoded_data = encode_data(data)
+
+        # Enviar los datos al servidor Kafka
+        producer.produce(topic, key='sensor20231', value=encoded_data)
         producer.flush()
 
         print(f"Datos enviados: {data}")
